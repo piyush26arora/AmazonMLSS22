3c9fe724491175df0e9dd662dd72e511b6299194
Piyush Arora <83628525+piyush26arora@users.noreply.github.com>
2022-07-08 20:44:06 +0530

    Add files via upload

---
+++ practice/demo.js
@@ -0,0 +1,11 @@
+// console.log(x)
+let x = 10 ;
+console.log(x)
+console.log(x*200)
+
+
+// declared but not assigned any value - undefined
+// not even declared - not defined 
+
+// not defined !== undefined 
+
